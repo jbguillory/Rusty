bf91a6e00455d3ca721f79e35523056c8e9bc4ae
John Guillory <john.guillory@secondfront.com>
2025-04-13 19:41:13 -0400

    rename folder

---
+++ projects/mars_calc/target/debug/deps/mars_calc-545c6676bc987e66.d
@@ -0,0 +1,5 @@
+/home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/target/debug/deps/mars_calc-545c6676bc987e66: src/main.rs
+
+/home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/target/debug/deps/mars_calc-545c6676bc987e66.d: src/main.rs
+
+src/main.rs:
