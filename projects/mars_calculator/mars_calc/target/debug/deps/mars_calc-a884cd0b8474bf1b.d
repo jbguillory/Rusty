8882b95424af87f20ca346d6124161e2c1c8cc33
John Guillory <john.guillory@secondfront.com>
2025-04-13 19:38:42 -0400

    push mars_calc

---
+++ projects/mars_calculator/mars_calc/target/debug/deps/mars_calc-a884cd0b8474bf1b.d
@@ -0,0 +1,5 @@
+/home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/target/debug/deps/libmars_calc-a884cd0b8474bf1b.rmeta: src/main.rs
+
+/home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/target/debug/deps/mars_calc-a884cd0b8474bf1b.d: src/main.rs
+
+src/main.rs:
---
+++ projects/mars_calculator/mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+/home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/target/debug/mars_calc: /home/johnguillory/workspace/personal/Rusty/projects/mars_calculator/mars_calc/src/main.rs
