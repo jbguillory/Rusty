d1005859b256c1b88ea83dd9af234b0492ca4659
John Guillory <john.guillory@secondfront.com>
2025-04-17 15:39:47 -0400

    push

--- projects/server/src/main.rs
+++ projects/server/src/main.rs
@@ -1,16 +1,6 @@
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-    let string_borrow: &str = &string;
-    let string_litteral = "1234";
-
-    dbg!(&string);
-    dbg!(string_slice);
-    dbg!(string_borrow);
-    dbg!(string_litteral);
-
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
 
 struct Server {
@@ -19,12 +9,28 @@ struct Server {
 
 impl Server {
     fn new(addr: String) -> Self { 
-        Self {
-            addr
-        }
+        Self { addr }
     }
 
     fn run(self) {
-
+        println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
